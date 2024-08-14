60499482b65ecf8196c735cbaf7218ed4cf6d52c
Ed Salinas <edsalinas7b@yahoo.com>
2024-08-14 11:45:47 +0800

    Implement basic VideoSDK at /video/index

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,12 +4,20 @@ import "./controllers"
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import HelloWorld from "./components/HelloWorld";
+import VideoSdkMeeting from "./components/VideoSdkMeeting";
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded event fired");
+  // Hello World
   const helloWorldDiv = document.getElementById("helloWorld");
   if (helloWorldDiv) {
-    ReactDOM.render(<HelloWorld />, helloWorldDiv);
+    createRoot(helloWorldDiv).render(<HelloWorld />);
+  }
+
+  // VideoSDK
+  const videoSdkMeetingDiv = document.getElementById("videoSdkMeetingDiv");
+  if (videoSdkMeetingDiv) {
+    createRoot(videoSdkMeetingDiv).render(<VideoSdkMeeting />);
   }
 });
---
+++ app/javascript/components/VideoSdkMeeting.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import {
+  MeetingProvider,
+  useMeeting,
+  useParticipant,
+} from "@videosdk.live/react-sdk";
+import ReactPlayer from "react-player";
+
+function ParticipantView(props) {
+  const micRef = useRef(null);
+  const { webcamStream, micStream, webcamOn, micOn, isLocal, displayName } =
+    useParticipant(props.participantId);
+
+  const videoStream = useMemo(() => {
+    if (webcamOn && webcamStream) {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(webcamStream.track);
+      return mediaStream;
+    }
+  }, [webcamStream, webcamOn]);
+
+  useEffect(() => {
+    if (micRef.current) {
+      if (micOn && micStream) {
+        const mediaStream = new MediaStream();
+        mediaStream.addTrack(micStream.track);
+
+        micRef.current.srcObject = mediaStream;
+        micRef.current
+          .play()
+          .catch((error) =>
+            console.error("videoElem.current.play() failed", error)
+          );
+      } else {
+        micRef.current.srcObject = null;
+      }
+    }
+  }, [micStream, micOn]);
+
+  return (
+    <div>
+      <audio ref={micRef} autoPlay playsInline muted={isLocal} />
+      {webcamOn && (
+        <ReactPlayer
+          playsinline // very very imp prop
+          pip={false}
+          light={false}
+          controls={false}
+          muted={true}
+          playing={true}
+          url={videoStream}
+          height={"300px"}
+          width={"300px"}
+          onError={(err) => {
+            console.log(err, "participant video error");
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+function MeetingView() {
+  const [joined, setJoined] = useState(null);
+  //Get the method which will be used to join the meeting.
+  //We will also get the participants list to display all participants
+  const { join, participants } = useMeeting({
+    //callback for when meeting is joined successfully
+    onMeetingJoined: () => {
+      setJoined("JOINED");
+    }
+  });
+  const joinMeeting = () => {
+    setJoined("JOINING");
+    join();
+  };
+
+  return (
+    <div className="container">
+      {joined && joined == "JOINED" ? (
+        <div>
+          {[...participants.keys()].map((participantId) => (
+            <ParticipantView
+              participantId={participantId}
+              key={participantId}
+            />
+          ))}
+        </div>
+      ) : joined && joined == "JOINING" ? (
+        <p>Joining the meeting...</p>
+      ) : (
+        <button onClick={joinMeeting}>Join the meeting</button>
+      )}
+    </div>
+  );
+}
+
+const VideoSdkMeeting = () => {
+ return (
+  <MeetingProvider
+  config={{
+    meetingId: "8ggn-da4u-lz82",
+    micEnabled: true,
+    webcamEnabled: true,
+    name: "Edwin's Org",
+  }}
+  token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI2YWE4NzMyMC0xZjk0LTRkZTQtYjFhYS01MjBmMDM2NjRiOGEiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcyMzYwMDg4NywiZXhwIjoxNzIzNjg3Mjg3fQ.zYdp-SMnvTdUg0Xts7vLRMAIZ-AZ0Y7APFscdi3MFaY"
+>
+  <MeetingView />
+</MeetingProvider>
+ )
+};
+export default VideoSdkMeeting;
